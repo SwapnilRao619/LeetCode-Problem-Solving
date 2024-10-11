d1c8a387ccc6964d12142ee7da1ba050c3703445
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2024-10-11 11:45:00 +0530

    Q20 solved.

--- Q20.py
+++ Q20.py
@@ -1,8 +1,10 @@
 ## N
 ## Idea, approach and time complexity:
-'''This code validates the correctness of brackets in a string using a stack. It iterates through the string, pushing opening brackets onto the stack and popping their 
+'''
+This code validates the correctness of brackets in a string using a stack. It iterates through the string, pushing opening brackets onto the stack and popping their 
 corresponding closing brackets if encountered. Any mismatch or an empty stack with a closing bracket returns False. If the stack is empty at the end, it returns True. 
-The time complexity is O(n), where n is the length of the input string.'''
+The time complexity is O(n), where n is the length of the input string.
+'''
 
 class Solution:
     def isValid(self, s: str) -> bool:
