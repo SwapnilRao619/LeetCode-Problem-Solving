7af5c20e36347633c0a2c837ad976176a820ca29
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2024-10-14 20:51:10 +0530

    Q206 solved.

---
+++ Q206.py
@@ -0,0 +1,18 @@
+## N
+## Idea, approach and time complexity:
+'''
+The provided code reverses a singly linked list iteratively by maintaining three pointers: `prev`, `curr`, and `temp`. The `curr` pointer traverses the list, and for 
+each node, it updates the `next` pointer to point to the `prev` node. This continues until the end of the list is reached, with the `prev` pointer ultimately pointing 
+to the new head of the reversed list. The time complexity is \(O(n)\), where \(n\) is the number of nodes in the list, as each node is processed once.
+'''
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=None
+        curr=head
+        while curr:
+            temp=curr.next
+            curr.next=prev
+            prev=curr
+            curr=temp
+        return prev
