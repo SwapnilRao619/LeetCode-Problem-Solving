21aac807ea49d96a83a35766765dd34fff366648
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2024-10-21 23:44:15 +0530

    Q232 solved.

--- Q232.py
+++ Q232.py
@@ -1,22 +1,23 @@
+## N
 ## Idea, approach and time complexity:
-'''This code implements a queue using a Python list. It supports push, pop, peek, and empty operations. Elements are added to the end of the list for push operations, 
-simulating the rear of the queue. Pop removes and returns the first element, peek returns the first element without removal, and empty checks if the queue is empty. 
-All operations have an average time complexity of O(1), except pop, which may take O(n) in the worst case due to list resizing.'''
+'''
+The `MyQueue` class implements a queue using a list. Elements are added to the end with `push`, and removed from the front with `pop`. The `peek` method retrieves the 
+front element without removing it, and `empty` checks if the queue is empty. The time complexity for `push` is O(1), while `pop` and `peek` are O(n) due to list removal 
+sand access operations. To improve efficiency, a deque or two stacks could be used instead.
+'''
 
-## Code:
 class MyQueue:
 
     def __init__(self):
-        self.s=[]
+       self.s=[] 
 
     def push(self, x: int) -> None:
         self.s.append(x)
 
     def pop(self) -> int:
-        if self.s:
-            v=self.s[0]
-            self.s.remove(self.s[0])
-            return v
+        p=self.s[0]
+        self.s.remove(self.s[0])
+        return p
 
     def peek(self) -> int:
         return self.s[0]
