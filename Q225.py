c730ee9d532a6dde27d3d06f5d6f7e74eecf40bc
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2024-10-21 21:07:00 +0530

    Q225 solved.

--- Q225.py
+++ Q225.py
@@ -1,22 +1,23 @@
+## N
 ## Idea, approach and time complexity:
-'''This code implements a stack using a Python list. It provides standard stack operations: push, pop, top, and empty. Elements are appended to the end of the list for 
-push operations, simulating the stack's top. Pop removes and returns the top element, top returns the top element without removal, and empty checks if the stack is empty.
-All operations have an average time complexity of O(1), except pop, which may take O(n) in the worst case due to list resizing.'''
+'''
+The `MyStack` class implements a stack using a deque to simulate stack operations. The `push` method adds elements to the back of the deque, while `pop` and `top` 
+involve rotating the deque to access the last element efficiently. The `empty` method checks if the deque is empty. The time complexity for `push` is O(1), while `pop` 
+and `top` have a time complexity of O(n) due to the need to rotate the elements.
+'''
 
-## Code:
 class MyStack:
 
     def __init__(self):
-        self.q=[]
+        self.q=deque()
 
     def push(self, x: int) -> None:
         self.q.append(x)
 
     def pop(self) -> int:
-        if self.q:
-            v=self.q[-1]
-            self.q.pop()
-            return v
+        for i in range(len(self.q)-1):
+            self.q.append(self.q.popleft())
+        return self.q.popleft()
 
     def top(self) -> int:
         return self.q[-1]
