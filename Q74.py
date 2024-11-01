2bce6ee1b34fcf5f811201ca96d3aaeb9319a944
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2024-11-01 19:04:09 +0530

    Q74 solved.

--- Q74.py
+++ Q74.py
@@ -1,33 +1,33 @@
+## N
 ## Idea, approach and time complexity:
-'''The code implements a binary search algorithm to search for the target element in a 2D matrix. It first determines the number of rows and columns in the matrix. 
-Then, it initializes two pointers, top and bot, to represent the top and bottom rows of the matrix, respectively. The algorithm iteratively updates these pointers to 
-narrow down the search space vertically based on the target value. Once it finds the row where the target might be located, it performs another binary search 
-horizontally within that row. If the target is found in the row, the function returns True; otherwise, it returns False. The approach efficiently searches for the 
-target element in the matrix using binary search, achieving O(log (n*m)) runtime complexity.'''
+'''
+The approach of this solution uses a two-step binary search on a 2D matrix. First, it identifies the correct row that may contain the target by comparing the target with 
+the first and last elements of each row. Once the potential row is found, a second binary search is performed within that row to find the target. This efficient method 
+leverages the sorted nature of the matrix, resulting in a time complexity of \(O(\log m + \log n)\), where \(m\) is the number of rows and \(n\) is the number of columns 
+in the matrix.
+'''
 
-## Code:
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows,cols=len(matrix), len(matrix[0]) 
-        top,bot=0,rows-1
-        while(top<=bot):
-            row=(top+bot)//2
-            if target>matrix[row][-1]:
-                top=row+1
-            elif target<matrix[row][0]:
-                bot=row-1
+        t,b=0,(len(matrix)-1)
+        while(t<=b):
+            r=(t+b)//2
+            if(matrix[r][-1]<target):
+                t=r+1
+            elif(matrix[r][0]>target):
+                b=r-1
             else:
                 break
-        if not (top<=bot):
+        if(not t<=b):
             return False
-        row=(top+bot)//2
-        l,r=0,cols-1
-        while(l<=r):
-            mid=(l+r)//2
-            if target>matrix[row][mid]:
-                l=mid+1
-            elif target<matrix[row][mid]:
-                r=mid-1
+        r=(t+b)//2
+        l,h=0,(len(matrix[0])-1)
+        while(l<=h):
+            m=(l+h)//2
+            if(matrix[r][m]<target):
+                l=m+1
+            elif(matrix[r][m]>target):
+                h=m-1
             else:
                 return True
         return False
