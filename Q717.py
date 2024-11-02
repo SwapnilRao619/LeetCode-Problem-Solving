2da30a8d9267ded4a24078ed52a4073cdcec45f0
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2024-11-02 22:24:55 +0530

    Q717 solved.

--- Q717.py
+++ Q717.py
@@ -1,18 +1,21 @@
 ## N
 ## Idea, approach and time complexity:
 '''
-The approach to determining if the last character in the `bits` list is a one-bit character involves iterating through the list while interpreting the bits. Each `0` 
-signifies a one-bit character, while each `1` indicates a two-bit character. As we traverse the list, we advance one position for a `0` and two positions for a `1`.
-After processing, we check if we land on the last bit of the list, which confirms whether it is indeed a one-bit character. The time complexity of this solution is O(n), 
-where n is the length of the `bits` list, since we only make a single pass through the array.
+The solution checks if the last character in a given binary array can be represented as a one-bit character. It iterates through the list, moving two indices forward 
+when encountering a '1' (indicating a two-bit character) and one index forward for a '0'. The function returns `True` if it ends at the last index after processing, 
+meaning the last character is indeed a one-bit character. The time complexity of this approach is O(n), where n is the length of the input list, as each element is 
+processed at most once.
 '''
 
 class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
         i=0
-        while(i<(len(bits)-1)):
+        while(i<len(bits)):
             if(bits[i]==1):
                 i+=2
             else:
-                i+=1
-        return i==(len(bits)-1)
+                if(i==len(bits)-1):
+                    return True
+                else:
+                    i+=1
+        return False    
