01868615e08ef3e9718e833e2933929a4c1b904f
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2024-12-07 21:11:30 +0530

    Q1 solved.

--- Q1.py
+++ Q1.py
@@ -3,7 +3,7 @@
 '''
 The given code solves the "Two Sum" problem by using a hash map to store the indices of the elements in the input list. It iterates through the list twice: the first 
 pass populates the hash map, and the second checks if the complement (target - current element) exists in the map while ensuring it's not the same index. The time 
-complexity is \(O(n)\) due to the linear scans, and the space complexity is also (O(n) for the hash map.
+complexity is (O(n) due to the linear scans, and the space complexity is also (O(n) for the hash map.
 '''
 
 class Solution:
