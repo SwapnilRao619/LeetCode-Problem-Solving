b6cb95cb051492ffc288d8544b5ed9cb02dd413e
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2025-01-25 15:49:26 +0530

    Q3110 solved.

--- Q3110.py
+++ Q3110.py
@@ -1,6 +1,18 @@
+## N
+## Idea, approach and time complexity:
+'''
+The given code calculates the "score" of a string by summing the absolute differences in ASCII values between each consecutive character. The approach iterates through 
+the string, comparing each pair of adjacent characters and adding the absolute difference to the score. The time complexity of this solution is O(n), where n is the 
+length of the string, as it processes each pair of characters once.
+'''
+
 class Solution:
-    def scoreOfString(self, s: str) -> int:
-        sc=0
-        for i in range(len(s)-1):
-            sc+=(abs(ord(s[i])-ord(s[i+1])))
-        return sc
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if(not root):
+            return
+        temp=root.right
+        root.right=root.left
+        root.left=temp
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+        return root
