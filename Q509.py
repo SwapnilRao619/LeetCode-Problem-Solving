0c249b5f054b48ad27fdfe89809fa677fd83539c
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2025-03-19 11:41:50 +0530

    Q509 solved.

--- Q509.py
+++ Q509.py
@@ -1,4 +1,4 @@
-## N
+## N 1
 ## Idea, approach and time complexity:
 '''
 The provided code implements a recursive solution to compute the nth Fibonacci number, where the Fibonacci sequence is defined as \( F(n) = F(n-1) + F(n-2) \) with base 
@@ -12,3 +12,23 @@ class Solution:
             return n
         else:
             return (self.fib(n-1)+self.fib(n-2))
+        
+## N 2
+## Idea, approach and time complexity:
+'''
+This solution uses **memoization** to optimize the Fibonacci calculation by storing previously computed values in a cache. The base cases are when `n` is 0 or 1, 
+returning `n` directly. For other values, the function checks the cache for precomputed results, and if not found, it recursively computes `fib(n-1)` and `fib(n-2)`, 
+storing the result. This approach reduces redundant computations, making the time complexity **O(n)**, as each Fibonacci number is computed only once and retrieved from 
+the cache in constant time.
+'''
+
+class Solution:
+    def __init__(self):
+        self.cache={}
+    def fib(self, n: int) -> int:
+        if(n==1 or n==0):
+            return n
+        if(n in self.cache):
+            return self.cache[n]
+        self.cache[n]=self.fib(n-1)+self.fib(n-2)
+        return self.cache[n]
