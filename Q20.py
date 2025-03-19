752beb8a4e50df2fc81a7cd5e93415318480e890
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2025-03-19 20:35:25 +0530

    Q20 solved.

--- Q20.py
+++ Q20.py
@@ -1,20 +1,22 @@
 ## N
 ## Idea, approach and time complexity:
 '''
-This code validates the correctness of brackets in a string using a stack. It iterates through the string, pushing opening brackets onto the stack and popping their 
-corresponding closing brackets if encountered. Any mismatch or an empty stack with a closing bracket returns False. If the stack is empty at the end, it returns True. 
-The time complexity is O(n), where n is the length of the input string.
+The function checks if a given string of parentheses is valid by using a stack. It iterates through the string, pushing opening brackets onto the stack and popping them 
+when a matching closing bracket is encountered. If a closing bracket doesn't match the top of the stack or the stack is empty when it shouldn't be, the function returns 
+`False`. Finally, it checks if the stack is empty to confirm all brackets were matched correctly. The time complexity is **O(n)**, where **n** is the length of the 
+string, as each character is processed once.
 '''
 
 class Solution:
     def isValid(self, s: str) -> bool:
         stack=[]
-        hm={"(":")","{":"}","[":"]"}
+        hm={')':'(','}':'{',']':'['}
         for p in s:
-            if p in hm:
-                stack.append(hm[p])
-            elif stack and stack[-1]==p:
-                stack.pop()
-            else:
-                return False
+            if(p=='(' or p=='{' or p=='['):
+                stack.append(p)
+            elif(p==')' or p=='}' or p==']'):
+                if(stack and stack[-1]==hm[p]):
+                    stack.pop()
+                else:
+                    return False
         return len(stack)==0
