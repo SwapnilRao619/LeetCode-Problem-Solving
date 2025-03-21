564facfed566978973826e10534e8d12d7de0bbc
Swapnil Rao <114131925+SwapnilRao619@users.noreply.github.com>
2025-03-21 15:04:49 +0530

    Q387 solved.

--- Q387.py
+++ Q387.py
@@ -1,20 +1,20 @@
 ## N
 ## Idea, approach and time complexity:
 '''
-The given code defines a method to find the index of the first non-repeating character in a string. It uses a dictionary to count occurrences of each character, marking 
-non-repeating characters with a value of 0. After counting, it checks for the first character with this count and returns its index. The time complexity is O(n), where 
-n is the length of the string, due to the two passes over the string: one for counting and another for finding the unique character.
+The solution uses a hash map to count the frequency of each character in the string. It first iterates through the string to populate the map, then checks for the first 
+character with a count of 1. If found, its index is returned; otherwise, -1 is returned. The time complexity is O(n) because the string is traversed twice, and the space 
+complexity is O(k), where k is the number of unique characters.
 '''
 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         hm={}
-        for i in range(len(s)):
-            if(s[i] in hm):
-                hm[s[i]]+=1
+        for i in s:
+            if(i not in hm):
+                hm[i]=1
             else:
-                hm[s[i]]=0
-        for k,v in hm.items():
-            if(v==0):
-                return s.find(k)
+                hm[i]+=1
+        for  i in range(len(s)):
+            if(hm[s[i]]==1):
+                return i
         return -1
